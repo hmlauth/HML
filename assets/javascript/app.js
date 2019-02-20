a2245aa4aaf67b3fe6b2b69fdcd8ed4f47bc10bb
Andromeda <hannalauth@gmail.com>
2019-02-19 17:16:08 -0800

    linked portfolios and added footer effect when scrolling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,28 @@
 $(document).ready(function () {
   // Materialize initialization for parallax
   $('.parallax').parallax();
+
 });
 
+// Footer effect
+$(window).scroll(function (event) {
+  function footer() {
+    var scroll = $(window).scrollTop();
+    if (scroll > 50 ) {
+      $(".page-footer").fadeOut("slow").addClass("show");
+    } else {
+      $(".page-footer").fadeIn("slow").removeClass("show");
+    }
+
+    clearTimeout($.data(this, 'scrollTimer'));
+
+    $.data(this, 'scrollTimer', setTimeout(function () {
+      if ($('.page-footer').is(':hover')) {
+        footer();
+      } else {
+        $(".page-footer").fadeIn("slow");
+      }
+    }, 2000));
+  }
+  footer();
+});
